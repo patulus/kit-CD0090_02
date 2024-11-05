a91604df6183ee400b61c83fdb6f47c0bdbb7670
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2024-11-05 17:55:27 +0900

    init

---
+++ w24w10CardDealer/src/main/kotlin/kr/ac/kumoh/s20210463/w24w10CardDealer/W24w10CardDealerApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20210463.w24w10CardDealer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24w10CardDealerApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24w10CardDealerApplication>(*args)
+}
--- w24w10CardDealer/src/main/kotlin/kr/ac/kumoh/s20210463/w24w10CardDealer/controller/CardController.kt
+++ w24w10CardDealer/src/main/kotlin/kr/ac/kumoh/s20210463/w24w10CardDealer/controller/CardController.kt
@@ -1,4 +1,16 @@
 package kr.ac.kumoh.s20210463.w24w10CardDealer.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
 class CardController {
+    @GetMapping("/cards/random")
+    fun getRandomCard(model: Model): String {
+        model.addAttribute("cards", "ace_of_spades.png")
+//        model.addAttribute("suit", "spades")
+//        model.addAttribute("rank", "2")
+        return "cards"
+    }
 }
