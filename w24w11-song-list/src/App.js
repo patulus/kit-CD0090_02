03bc1e4e21f059cb26ee980edec4fd3511a4ebb9
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2024-11-26 17:36:24 +0900

    🚧 Lambda Expression 사용

--- w24w11-song-list/src/App.js
+++ w24w11-song-list/src/App.js
@@ -1,24 +1,20 @@
 import './App.css'
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Playlist />
-      <Container title="비밀번호 486" index="1" />
-      <Container title="사건의 지평선" index="2" />
-      <Container title="Supersonic" index="3" />
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <Header />
+    <Playlist />
+    <Container title="비밀번호 486" index="1" />
+    <Container title="사건의 지평선" index="2" />
+    <Container title="Supersonic" index="3" />
+  </div>
+)
 
-function Header() {
-  return (
+const Header = () => (
     <h1>React 프로그래밍</h1>
-  )
-}
+)
 
-function Playlist() {
+const Playlist = () => {
   return (
     // 자바스크립트에 class 키워드가 있어 className을 사용함
     <div className="playlist">프로그래밍하면서 듣고 싶은 노래</div>
