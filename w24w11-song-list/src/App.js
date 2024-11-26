87713b905c6476c80a32a97c455519787fd47cfb
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2024-11-26 17:44:14 +0900

    ✂️ Container Component 분리

--- w24w11-song-list/src/App.js
+++ w24w11-song-list/src/App.js
@@ -1,4 +1,5 @@
 import './App.css'
+import Container from './Container.js'
 
 const App = () => (
   <div>
@@ -21,15 +22,6 @@ const Playlist = () => {
   )
 }
 
-// 고객의 요구가 변경될 수 있으므로 최소한으로 단기간 내 변경할 수 있어야 함
-const Container = (props) => (
-  <div className="container">
-    <a href={`https://www.youtube.com/results?search_query=${props.title.replaceAll(" ", "+")}`}>
-    <img src={`https://picsum.photos/600/150?random=${props.index}`} alt={`랜덤 이미지 ${props.index}`} />
-      {/* <img src={`https://picsum.photos/600/150?title=${props.title}`} alt={`${props.title}`} /> */}
-      <div className="song-title">{props.title}</div>
-    </a>
-  </div>
-)
+// Container의 흔적...
 
 export default App
