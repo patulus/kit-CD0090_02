41923e3916470642cfd65db9de27bced543e22e2
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2024-11-27 15:25:39 +0900

    🍇 노래 제목 배열 사용

--- w24w11-song-list/src/App.js
+++ w24w11-song-list/src/App.js
@@ -1,24 +1,38 @@
 import './App.css'
 import Container from './Container.js'
 
-const App = () => (
-  <div>
-    <Header />
-    <Playlist />
-    <Container title="비밀번호 486" index="1" />
-    <Container title="사건의 지평선" index="2" />
-    <Container title="Supersonic" index="3" />
-  </div>
-)
+const App = () => {
+  const songs = [
+    "비밀번호 486",
+    "사건의 지평선",
+    "Supersonic",
+  ]
+
+  return (
+    <div>
+      <Header />
+      <Playlist title="프로그래밍하면서 듣고 싶은 노래" listSong={songs} />
+    </div>
+  )
+}
 
 const Header = () => (
     <h1>React 프로그래밍</h1>
 )
 
-const Playlist = () => {
+const Playlist = (props) => {
   return (
     // 자바스크립트에 class 키워드가 있어 className을 사용함
-    <div className="playlist">프로그래밍하면서 듣고 싶은 노래</div>
+    <div className="playlist">
+      <h2>{props.title}</h2>
+      {
+        props.listSong.map(
+          (song, index) => (
+            <Container key={index} title={song} index={index} />
+          )
+        )
+      }
+    </div>
   )
 }
 
