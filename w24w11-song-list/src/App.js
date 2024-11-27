f2dcf2c796890ed43d1b89bde0e627ff5ad3de3b
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2024-11-27 16:06:45 +0900

    💄 노래 정보 배열 사용

--- w24w11-song-list/src/App.js
+++ w24w11-song-list/src/App.js
@@ -3,16 +3,41 @@ import Container from './Container.js'
 
 const App = () => {
   const songs = [
-    "비밀번호 486",
-    "사건의 지평선",
-    "Supersonic",
+    {
+      id: 1,
+      title: "사랑에 연습이 있었다면",
+      singer: "임재현",
+      rating: 5,
+      lyrics: `사랑에 연습이 있었다면
+우리는 달라졌을까
+내가 널 만난 시간 혹은 그 장소
+상황이 달랐었다면 우린 맺어졌을까
+하필 넌 왜 내가 그렇게 철없던 시절에
+나타나서 그렇게 예뻤니
+너처럼 좋은여자가 왜 날 만나서 그런
+과분한 사랑 내게 줬는지`,
+    },
+    {
+      id: 2,
+      title: "사건의 지평선",
+      singer: "윤하",
+      rating: 3,
+      lyrics: null,
+    },
+    {
+      id: 3,
+      title: "사랑은 늘 도망가",
+      singer: "임영웅",
+      rating: 3,
+      lyrics: null,
+    },
   ]
 
   return (
-    <div>
+    <>
       <Header />
       <Playlist title="프로그래밍하면서 듣고 싶은 노래" listSong={songs} />
-    </div>
+    </>
   )
 }
 
@@ -26,9 +51,8 @@ const Playlist = (props) => {
     <div className="playlist">
       <h2>{props.title}</h2>
       {
-        props.listSong.map(
-          (song, index) => (
-            <Container key={index} title={song} index={index} />
+        props.listSong.map(song => (
+            <Container song={song} />
           )
         )
       }
--- w24w11-song-list/src/Container.js
+++ w24w11-song-list/src/Container.js
@@ -1,12 +1,19 @@
 // 고객의 요구가 변경될 수 있으므로 최소한으로 단기간 내 변경할 수 있어야 함
 const Container = (props) => (
-    <div className="container">
-        <a href={`https://www.youtube.com/results?search_query=${props.title.replaceAll(" ", "+")}`}>
-            <img src={`https://picsum.photos/600/150?random=${props.index}`} alt={`랜덤 이미지 ${props.index}`} />
-            {/* <img src={`https://picsum.photos/600/150?title=${props.title}`} alt={`${props.title}`} /> */}
-            <div className="song-title">{props.title}</div>
-        </a>
-    </div>
+    <>
+        <div className="container">
+            <a href={`https://www.youtube.com/results?search_query=${props.song.title.replaceAll(" ", "+")}`} target="_blank" rel="noreferrer">
+                <img src={`https://picsum.photos/600/150?random=${props.song.id}`} alt={`랜덤 이미지 ${props.song.id}`} />
+                {/* <img src={`https://picsum.photos/600/150?title=${props.title}`} alt={`${props.title}`} /> */}
+                <div className="song-title">{`${props.song.title} (${props.song.singer})`}</div>
+            </a>
+        </div>
+        {props.song.lyrics && (
+            <pre className="song-lyrics">
+                {props.song.lyrics}
+            </pre>
+        )}
+    </>
 )
 
 export default Container
